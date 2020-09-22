04537de3ccb47e8b173fcd9b92184f63aefa259d
Edgar Omar Monreal <edgar.monreal7686@alumnos.udg.mx>
2020-09-21 19:59:51 -0500

    Se agregó la función de 'Estructuras de personajes' y se concluyó el proyecto con exito

--- Actividad04.cpp
+++ Actividad04.cpp
@@ -32,6 +32,39 @@ void mostrar(int n, char arreglo[]) {
   }
   cout << "\n" << endl;
 }
+
+void personajes() {
+  struct Personaje
+  {
+    char nombre[30];
+    char tipo[10];
+    char fuerza;
+    char salud[15];
+  } personajes[5];
+  
+  for (int i = 0; i < 5; i++) {
+    fflush(stdin);
+    cout << "\n***** Personaje " << i + 1 << " *****\n";
+    cout << "Ingresa el nombre del personaje: "; cin.getline(personajes[i].nombre, 30, '\n');
+    fflush(stdin);
+    cout << "Ingresa el tipo del personaje: "; cin.getline(personajes[i].tipo, 10, '\n');
+    fflush(stdin);
+    cout << "Ingresa la fuerza del personaje: "; cin >> personajes[i].fuerza;
+    fflush(stdin);
+    cout << "Ingresa la salud del personaje: "; cin.getline(personajes[i].salud, 15, '\n');
+  }
+ 
+  cout << "\n----------- Visualizando personajes -----------" << endl;
+ 
+  for (int i = 0; i < 5; i++) {
+    cout << "\n***** Personaje " << i + 1 << " *****\n";
+    cout << "El nombre del personaje es: " << personajes[i].nombre << "\n";
+    cout << "El tipo del personaje es: " << personajes[i].tipo << "\n";
+    cout << "La fuerza del personaje es: " << personajes[i].fuerza << "\n";
+    cout << "La salud del personaje es: " << personajes[i].salud << "\n";
+  }
+  cout << endl;
+}
  
 int main() {
   int opc = 0;
@@ -58,7 +91,7 @@ int main() {
       mostrar(n, arreglo);
     }
     break;
-    case 3: break;
+    case 3: personajes(); break;
     case 4:
       cout << "\nSaliendo del programa...";
       return 0;
